cd145ee8de736e060f4850f196f00010a192f139
JiSooMoon <36117303+JiSooMoon@users.noreply.github.com>
2018-07-10 09:01:09 -0400

    Add files
    This is the last commit that finalizes work of the applicaiton

---
+++ SubActivity.java
@@ -0,0 +1,23 @@
+package com.example.jisoo.activity_change;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_sub);
+        TextView nameText = (TextView) findViewById(R.id.nameText);
+        Intent intent = getIntent();
+        nameText.setText(intent.getStringExtra("nameText").toString());
+    }
+
+}
